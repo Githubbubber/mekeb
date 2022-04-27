46515bf9e0971c1181c9af878ba0d59697312806
Meke Brown <mekesiabrown@gmail.com>
2022-04-27 13:35:44 -0400

    Add three packages; Update Pug code

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require('path');
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'views')));
 
 app.set('view engine', 'pug');
 
 app.get("/", (req, res) => {
-  res.render('index', { title: 'Mekesia Brown', message: 'Hello there!' });
+  res.render('index', { title: 'Mekesia Brown' });
 });
 
 app.get("/affirm", (req, res) => {
